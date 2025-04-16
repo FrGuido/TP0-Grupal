c32c551a653afbea4adc498384a5dd764a21fc10
FrGuido <frguido@uade.edu.ar>
2025-04-16 16:36:58 -0300

    Cambios al login y a los esqueletos

---
+++ login.py
@@ -0,0 +1,42 @@
+import main
+
+
+def login():
+
+    #definir usuario admin
+    admin = 'admin'
+    adminpasww = 'admin'
+
+    #inicializo tipo de usuario a retornar
+    usertype = ''
+
+    while True:
+        user = input('Ingrese su DNI: ')
+        if user == admin:
+            usertype = 'admin'
+
+        elif any(x ['dni'] == user for x in main.profesores):
+                usertype = 'p'
+                break
+        
+        elif any(x ['dni'] == user for x in main.alumnos):
+            break
+
+        else:
+             print('Ha ingresado un DNI no valido, intente de nuevo\n')
+
+    while True:
+        pasww = input('Ingrese su contraseña: ')
+        if user == admin:
+            usertype = 'admin'
+
+        elif any(x ['pasw'] == user for x in main.profesores):
+                usertype = 'p'
+                break
+        
+        elif any(x ['pasw'] == user for x in main.alumnos):
+            break
+
+        else:
+             print('Ha ingresado un DNI no valido, intente de nuevo\n')
+    return
